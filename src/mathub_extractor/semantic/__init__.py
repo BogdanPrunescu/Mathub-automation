21e40f7ebd15c01dd0940e2ec1c74e6ea1bd9f39
BogdanPrunescu <87444439+BogdanPrunescu@users.noreply.github.com>
2026-09-12 09:49:17 +0200

    Add semantic retrieval package

---
+++ src/mathub_extractor/semantic/__init__.py
@@ -0,0 +1,5 @@
+"""Experimental local semantic retrieval for lesson localization."""
+
+from .index import DEFAULT_MODEL_NAME, SEMANTIC_INDEX_VERSION, search_manual
+
+__all__ = ["DEFAULT_MODEL_NAME", "SEMANTIC_INDEX_VERSION", "search_manual"]
